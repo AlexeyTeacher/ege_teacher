cffbb5a5b1b5c813ba7355f35ecc901e77d86601
AlexeyTeacher <ak45rus@gmail.com>
2022-04-05 04:40:56 +0300

    add auth and logout

--- ege_task/views.py
+++ ege_task/views.py
@@ -1,9 +1,11 @@
+from django.contrib.auth import logout, login
+from django.contrib.auth.views import LoginView
 from django.http import HttpResponseNotFound, Http404
 from django.shortcuts import render, redirect, get_object_or_404
 from django.urls import reverse_lazy
 from django.views.generic import ListView, DetailView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from .forms import AddTaskForm, RegisterUserForm
+from .forms import AddTaskForm, RegisterUserForm, LoginUserForm
 from .models import *
 from .utils import *
 
@@ -71,10 +73,6 @@ def contact(request):
     return render(request, "ege_task/contact.html", {'title': "Обратная связь"})
 
 
-def login(request):
-    return render(request, "ege_task/login.html", {'title': "Авторизация"})
-
-
 class EgeTaskCategory(DataMixin, ListView):
     model = Task
     template_name = 'ege_task/index.html'
@@ -102,3 +100,26 @@ class RegisterUser(DataMixin, CreateView):
         c_def = self.get_user_context(title='Регистрация')
         context.update(c_def)
         return context
+
+    def form_valid(self, form):
+        user = form.save()
+        login(self.request, user)
+        return redirect('home')
+
+class LoginUser(DataMixin, LoginView):
+    form_class = LoginUserForm
+    template_name = 'ege_task/login.html'
+
+    def get_context_data(self, *, object_list=None, **kwargs):
+        context = super().get_context_data(**kwargs)
+        c_def = self.get_user_context(title='Авторизация')
+        context.update(c_def)
+        return context
+
+    def get_success_url(self):
+        return reverse_lazy('home')
+
+
+def logout_user(request):
+    logout(request)
+    return redirect('login')
